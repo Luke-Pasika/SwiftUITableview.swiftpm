369b87acd83cc6ffca5f64d6040cee0053193898
Luke Pasika <Lukepasika@gmail.com>
2023-08-22 10:57:21 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("Darth Vader")
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Yo Dudes, the Empires Pretty Chill, you should like Join it or something!")
+        }
+    }
+}
